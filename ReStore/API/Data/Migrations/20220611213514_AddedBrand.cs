d62b24a9210fd1264b82ce796bffb63cc2dd3f2c
Mahmoud Sabbagh <mahmoudsabbagh8@gmail.com>
2022-06-12 01:32:31 -0400

    End of Section 2

---
+++ ReStore/API/Data/Migrations/20220611213514_AddedBrand.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    public partial class AddedBrand : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PictureURL",
+                table: "Products",
+                newName: "PictureUrl");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Brand",
+                table: "Products",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Brand",
+                table: "Products");
+
+            migrationBuilder.RenameColumn(
+                name: "PictureUrl",
+                table: "Products",
+                newName: "PictureURL");
+        }
+    }
+}
